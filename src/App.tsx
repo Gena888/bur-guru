b0c259229ff621dafe9ced4d6418294e0a2f52c8
Gena888 <TheGena888@gmail.com>
2021-02-23 17:31:46 +0300

    feat: create brouse

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Header } from './components/header/header';
+import { Route, withRouter, BrowserRouter, Redirect, Switch, HashRouter } from 'react-router-dom';
+import { Welcome } from './components/welcome/welcome';
+import { Production } from './components/production/production';
+import { PriseList } from './components/production/prise-list/prise-list';
+import { Procecess } from './components/process/process';
+import { SandBlasting } from './components/process/sandblasting/sandblasting';
+import { Delivery } from './components/delivery/delivery';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' render={() => <Redirect to={'/Welcome'} />} />
+          <Route path='/Welcome' render={() => <Welcome />} />
+          <Route path='/Production' render={() => <Production />} />
+          <Route path='/PriseList' render={() => <PriseList />} />
+          <Route path='/Procecess' render={() => <Procecess/>} />
+          <Route path='/SandBlasting' render={() => <SandBlasting />} />
+          <Route path='/Delivery' render={() => <Delivery />} />
+          <Route exact path='*' render={() => <div>404 not found</div>} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
